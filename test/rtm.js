6080bff10616cce92d12cb450bad16ba891a86d4
Michael Schoonmaker <michael.schoonmaker@redrobotlabs.com>
2012-07-16 22:22:30 -0700

    Finished auth and a couple simple methods.

---
+++ test/rtm.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect,
+    RTMClient = require('../lib/rtm');
+
+describe('RTMClient', function () {
+    describe('getAPISig', function () {
+        it('should return a valid MD5 hash');
+        it('should ignore key order in options');
+    });
+});
